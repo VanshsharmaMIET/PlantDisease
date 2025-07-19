131db8ce773bed641c2b71518816e3ad8d88a6d8
VanshsharmaMIET <137602022+VanshsharmaMIET@users.noreply.github.com>
2025-07-19 11:48:18 +0530

    Delete AgriGuard_ML-main directory

--- AgriGuard_ML-main/crop_recommendation/crop_model.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-
-# Would need to add the model file
-def crop_classifier():
-    crop_recommendation_model_path = 'models/RandomForest.pkl'
-    crop_recommendation_model = pickle.load(
-        open(crop_recommendation_model_path, 'rb'))
-    return crop_recommendation_model
